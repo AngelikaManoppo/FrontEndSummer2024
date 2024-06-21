74e4577dd048e318366e84bceebc0697d9e64d13
angelikaManoppo <s22210205@student.unklab.ac.id>
2024-06-21 09:07:22 +0800

    Update merge conflict 2

--- pertemuan-14/index.js
+++ pertemuan-14/index.js
@@ -2,3 +2,4 @@ console.log("pertemuyan 14");
 console.log("hello git anf github");
 console.log("testing merge conflict dari vs code")
 console.log("testing merge conflict from github")
+console.log("testing merge conflict dari vs code 2")
